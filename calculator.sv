06f49eb48787bb8ee548b412e96a8d8cfa739fb9
TavidTerzian <davidterzian8@yahoo.com>
2022-02-03 14:31:00 -0800

    num and op go high when needed. Val1 and 2 still dont get set

--- calculator.sv
+++ calculator.sv
@@ -28,7 +28,11 @@ module calculator(
     input clr,
     input [2:0] opcode,
     input [9:0] btn,
-    output reg [31:0] displayedNum
+    output logic num,
+    output logic op,
+    output logic [31:0] displayedNum,
+    output logic [31:0] val1,
+    output logic [31:0] val2
     );
     
     localparam [4:0] s0 = 0,
@@ -41,40 +45,59 @@ module calculator(
                     s7 = 7,
                     s8 = 8;
                         
-    reg [4:0] state = s0;
-    reg [31:0] val1;
-    reg [31:0] val2;
+    reg [3:0] state = s0;
+    //reg [31:0] val1;
+    //reg [31:0] val2;
     reg [3:0] pressedNum;
     reg [2:0] pressedOp;
     reg [2:0] prevOp;
-    logic op;
-    logic num;
+    //logic op;
+    //logic num;  
     
+    always_comb begin
+        if (btn != 0) num = 1;
+        else num = 0;
+    end   
     
-    always @ (posedge clk) begin
-        if (opcode == 0) op = 0;
-        else op = 1;
+    always_comb begin
+        if (opcode != 0) op = 1;
+        else op = 0;
     end
     
-    always @ (posedge clk) begin
-        if(btn == 0) num = 0;
-        else num = 1;
+    always_comb begin
+        case (state)
+            s1:begin
+                val1=0;
+                val2=0;
+                pressedNum = 0;
+                pressedOp = 0;
+                prevOp = 0;
+            end
+            s3: pressedNum = getNum(btn);
+            s4:begin
+               val1 = pressedNum + val1*10;
+               displayedNum = val1;
+            end
+            s5: begin
+                prevOp = pressedOp;
+                pressedOp = opcode;
+            end
+            s6: val2 = eval(val1, val2, prevOp);
+            s7: begin
+                val1=val1*-1;
+                displayedNum = val1;
+            end
+        endcase
     end
-      
-    always @ (posedge clk)begin
+  
+    always @ (posedge clk)begin        
         if(rst) state = s0;
         else 
         begin
             case(state) 
                 s0: if(pwr) state = s1;
-                s1: begin
-                    val1=0;
-                    val2=0;
-                    pressedNum = 0;
-                    pressedOp = 0;
-                    prevOp = 0;
-                    state = s2;
-                end
+                    else state = s0;
+                s1: state = s2;
                 s2:begin
                     if(clr) state = s1;
                     else if(num)state = s3;
@@ -83,30 +106,16 @@ module calculator(
                     else state = s2;
                 end
                 s3: begin
-                    pressedNum = getNum(btn);
                     if(!num) state = s4;
                     else state = s3;
                 end
-                s4:begin 
-                    val1 = pressedNum + val1*10;
-                    displayedNum = val1;
-                    state = s2;
-                end
+                s4: state = s2;
                 s5:begin
-                    prevOp = pressedOp;
-                    pressedOp = opcode;
                     if(!op) state = s6;
                     else state = s5;
                 end
-                s6:begin
-                    val2 = eval(val1, val2, prevOp);
-                    state = s2;
-                end
-                s7:begin
-                    val1=val1*-1;
-                    displayedNum = val1;
-                    state = s2;
-                end
+                s6: state = s2;
+                s7: state = s2;
                 default: state = s0;
             endcase
         end
@@ -122,6 +131,7 @@ module calculator(
             3'b011: return val1-val2;
             3'b100: return val2/val1;
             3'b101: return val1*val2;
+            default: return 0;
         endcase
     endfunction
     
@@ -129,7 +139,7 @@ module calculator(
         case(btn)
             10'b0000000001: return 0;
             10'b0000000010: return 1;
-            10'b000000100: return 2;
+            10'b0000000100: return 2;
             10'b0000001000: return 3;
             10'b0000010000: return 4;
             10'b0000100000: return 5;
@@ -137,6 +147,7 @@ module calculator(
             10'b0010000000: return 7;
             10'b0100000000: return 8;
             10'b1000000000: return 9;
+            default: return 0;
         endcase
     endfunction
     
--- calculator_tb.sv
+++ calculator_tb.sv
@@ -30,10 +30,14 @@ module calculator_tb(
     logic [9:0] btn;
     
     //output
+    logic num;
+    logic op;
+    logic [31:0] val1;
+    logic [31:0] val2;
     logic [31:0] displayedNum;
     
     calculator uut(
-    .pwr(pwr), .opcode(opcode), .btn(btn), .displayedNum(displayedNum)
+    .pwr(pwr), .opcode(opcode), .btn(btn), .displayedNum(displayedNum), .num(num), .op(op), .val1(val1), .val2(val2)
     );
     
     initial
@@ -46,28 +50,28 @@ module calculator_tb(
                 pwr = 1;
             end
             if(i == 3)begin
-                pwr = 0;
+                pwr = 0;    
             end
             if(i == 5)begin
                 btn = 10'b0000100000;
             end
-            if(i == 9)begin
+            if(i == 15)begin
                 btn = 10'b0000000000;
-            end
-            if(i == 13)begin
-                opcode = 3'b010;
-            end
-            if (i == 17) opcode = 3'b000;
-            if(i == 20)begin
-                btn = 10'b0000001000;
             end
             if(i == 25)begin
+                opcode = 3'b010;
+            end
+            if (i == 35) opcode = 3'b000;
+            if(i == 45)begin
+                btn = 10'b0000001000;
+            end
+            if(i == 55)begin
                 btn = 10'b0000000000;
             end
-            if(i == 30)begin
+            if(i == 65)begin
                 opcode = 3'b001;
             end
-            if(i == 35) opcode = 3'b000;
+            if(i == 75) opcode = 3'b000;
             
         end
     
